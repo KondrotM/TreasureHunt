4b208d29b0b68377e4e2627dc37aa9715806d10a
kondrotM <kondrot.m@gmail.com>
2020-02-06 15:52:52 +0000

    pageScreen works

--- screens/PageScreen.js
+++ screens/PageScreen.js
@@ -5,16 +5,11 @@ import { ExpoLinksView } from '@expo/samples';
 export default function LinksScreen() {
   return (
     <ScrollView style={styles.container}>
-      {/**
-       * Go ahead and delete ExpoLinksView and replace it with your content;
-       * we just wanted to provide you with some helpful links.
-       */}
-    <Text>
-      Here is a page
-    </Text>
+
     </ScrollView>
   );
 }
+
 
 LinksScreen.navigationOptions = {
   title: 'Links',
