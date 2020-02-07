bda50fd8ba65fcd04cf79949e68f7109e99fb208
Oscar Nardone <PaintNinja@users.noreply.github.com>
2020-02-07 15:42:47 +0000

    Taskbar navigation panels, code cleanup
    Also includes colour scheme for the taskbar and iconography

--- App.js
+++ App.js
@@ -1,62 +1,98 @@
-import { AppLoading } from 'expo';
-import { Asset } from 'expo-asset';
-import * as Font from 'expo-font';
-import React, { useState } from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import * as React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-import AppNavigator from './navigation/AppNavigator';
+// Navbar and headers
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = useState(false);
+// Iconography
+import { FontAwesome } from '@expo/vector-icons';
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return (
-      <AppLoading
-        startAsync={loadResourcesAsync}
-        onError={handleLoadingError}
-        onFinish={() => handleFinishLoading(setLoadingComplete)}
-      />
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <AppNavigator />
-      </View>
-    );
-  }
-}
-
-async function loadResourcesAsync() {
-  await Promise.all([
-    Asset.loadAsync([
-      require('./assets/images/robot-dev.png'),
-      require('./assets/images/robot-prod.png'),
-    ]),
-    Font.loadAsync({
-      // This is the font that we are using for our tab bar
-      ...Ionicons.font,
-      // We include SpaceMono because we use it in HomeScreen.js. Feel free to
-      // remove this if you are not using it in your app
-      'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-    }),
-  ]);
-}
-
-function handleLoadingError(error) {
-  // In this case, you might want to report the error to your error reporting
-  // service, for example Sentry
-  console.warn(error);
-}
-
-function handleFinishLoading(setLoadingComplete) {
-  setLoadingComplete(true);
-}
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
+  text: {
+    color: '#fff',
+  }
 });
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is the Home screen</Text>
+      <Button title="Go to Details" onPress={() => navigation.navigate('Details')}></Button>
+    </View>
+  );
+}
+
+function CreateScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is the Create screen</Text>
+    </View>
+  );
+}
+
+function SocialScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>This is the Social screen</Text>
+    </View>
+  );
+}
+
+export default function BottomTabs() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+      // Colours
+        tabBarOptions={{
+            activeTintColor: '#000', inactiveTintColor: '#777', activeBackgroundColor: '#caf7e2', inactiveBackgroundColor: '#caf7e2'
+        }}
+
+      // Icons
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === "Home") {
+              iconName = focused ? 'map' : 'map-o';
+            } else if (route.name === "Create") {
+              iconName = focused ? 'plus-square' : 'plus-square-o';
+            } else if (route.name === "Social") {
+              iconName = focused ? 'user' : 'user-o';
+            }
+
+            return <FontAwesome name={iconName} color={color} size={size} />
+          }
+        })}>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Create" component={CreateScreen} />
+        <Tab.Screen name="Social" component={SocialScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} options=
+          {{
+            headerStyle: {backgroundColor: '#3d3522'},
+            headerTintColor: '#fff',
+          }} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
