bf8520086c41daa4b0839b3e00b879eecc40b1ea
Matej Kondrot <kondrot.m@gmail.com>
2020-02-14 14:09:32 +0000

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 6a5bc43c9bedd90bbf7541c7ef63ca6397fb4563, reversing
    changes made to cf48d96cc5eccd236ba8902091355700f66807cb.

--- install-libs.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-npm install -g expo-cli
-npm install react-navigation
-npm install react-native-maps
-npm install @expo/vector-icons
-npm install @react-navigation/bottom-tabs
-npm install
-pause
